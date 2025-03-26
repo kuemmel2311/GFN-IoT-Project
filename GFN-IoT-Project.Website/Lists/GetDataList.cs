9878a9e44722ce8848f35dd87d9152b3c987dd68
Flying Phoenix <andrei081993@gmail.com>
2025-03-26 18:09:15 +0100

    (ADD) Custom Data Lists

--- GFN-IoT-Project.Website/Lists/GetDataList.cs
+++ GFN-IoT-Project.Website/Lists/GetDataList.cs
@@ -4,8 +4,47 @@ namespace GFN_IoT_Project.Lists
 {
     public class GetDataList
     {
-        public int ID { get; set; }
-        public required DateAndTime DateAndTime { get; set; }
-        public required int DataValue {  get; set; } 
+        public class TemperatureData
+        {
+            public int Id { get; set; }
+            public DateTime Timestamp { get; set; }
+            public double Temperature { get; set; }
+        }
+
+        public class HumidityData
+        {
+            public int Id { get; set; }
+            public DateTime Timestamp { get; set; }
+            public double Humidity { get; set; }
+        }
+
+        public class PressureData
+        {
+            public int Id { get; set; }
+            public DateTime Timestamp { get; set; }
+            public double Pressure { get; set; }
+        }
+
+        public class AirQualityData
+        {
+            public int Id { get; set; }
+            public DateTime Timestamp { get; set; }
+            public double AirQuality { get; set; }
+        }
+
+        public class LightLevelData
+        {
+            public int Id { get; set; }
+            public DateTime Timestamp { get; set; }
+            public double LightLevel { get; set; }
+        }
+
+        public class ApiKey
+        {
+            public int Id { get; set; }
+            public string Key { get; set; }
+            public string Owner { get; set; }
+            public DateTime CreatedAt { get; set; }
+        }
     }
 }
