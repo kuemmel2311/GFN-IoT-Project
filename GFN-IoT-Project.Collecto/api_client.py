74acbad864242d51d434efc1de7c78b5648a45c9
Flying Phoenix <andrei081993@gmail.com>
2025-03-27 15:35:52 +0100

    Update api_client.py

--- GFN-IoT-Project.Collecto/api_client.py
+++ GFN-IoT-Project.Collecto/api_client.py
@@ -1,90 +1,41 @@
-
-import requests     # Zur Kommunikation mit der API
-import config       # Einfuegen der config-Datei
+import requests
+import config
 
 
 class API_Request:
 
     def Statuscode(code):
-        if code == 200:
-            print("Übertragung erfolgreich!")
-        elif code == 403:
-            print("Falscher Authentifizierungscode!")
-        elif code == 404:
-            print("Seite wurde nicht gefunden!")
-        elif code == 500:
-            print("HTTP Fehler: Interner Serverfehler!")
-        else:
-            print(f"Statuscode: {code}")    
+        messages = {
+            200: "Übertragung erfolgreich!",
+            403: "Falscher Authentifizierungscode!",
+            404: "Seite wurde nicht gefunden!",
+            500: "HTTP Fehler: Interner Serverfehler!"
+        }
+        print(messages.get(code, f"Statuscode: {code}"))
 
+    def send_data(endpoint, data):
+        try:
+            dataValue = {"dataValue": data}
+            headers = {
+                "APIKey": config.API_SEND_DATA.auth_token,
+                "Content-Type": "application/json"
+            }
+            response = requests.post(endpoint, json=dataValue, headers=headers)
+            API_Request.Statuscode(response.status_code)
+        except requests.exceptions.RequestException as e:
+            print(f"Request error: {e}")
 
     def send_humidity(data):
-        try:
-
-            dataValue = {"dataValue": data}                                                             # Sensordaten die gesendet werden sollen --> Feuchtigkeit
-
-            headers = {"APIKey": config.API_SEND_DATA.auth_token,                                       # Authentifizierung nötig zur Eingabe von Daten
-                    "Content-Type": "application/json"}
-        
-            response = requests.post(config.Client.SEND_HUMIDITY_DATA, json=dataValue, headers=headers) # Befehl zum Senden an die API
-                                                                                                        
-            API_Request.Statuscode(response.status_code)
-        except requests.exceptions.RequestException as e:
-            API_Request.Statuscode(e.response.status_code)
-
+        API_Request.send_data(config.Client.SEND_HUMIDITY_DATA, data)
 
     def send_pressure(data):
-        try:
-
-            dataValue = {"dataValue": data}                                                             # Sensordaten die gesendet werden sollen -->  Luftdruck
-        
-            headers = {"APIKey": config.API_SEND_DATA.auth_token,                                       # Authentifizierung nötig zur Eingabe von Daten
-                   "Content-Type": "application/json"}
-        
-            response = requests.post(config.Client.SEND_PRESSURE_DATA, json=dataValue, headers=headers) # Befehl zum Senden an die API
-        
-            API_Request.Statuscode(response.status_code)
-        except requests.exceptions.RequestException as e:
-            API_Request.Statuscode(e.response.status_code)
+        API_Request.send_data(config.Client.SEND_PRESSURE_DATA, data)
 
     def send_temp(data):
-        try:
-
-            dataValue = {"dataValue": data}                                                             # Sensordaten die gesendet werden sollen --> Temperatur
-
-            headers = {"APIKey": config.API_SEND_DATA.auth_token,                                       # Authentifizierung nötig zur Eingabe von Daten
-                   "Content-Type": "application/json"}
-        
-            response = requests.post(config.Client.SEND_TEMP_DATA, json=dataValue, headers=headers)     # Befehl zum Senden an die API
-
-            API_Request.Statuscode(response.status_code)
-        except requests.exceptions.RequestException as e:
-            API_Request.Statuscode(e.response.status_code)
+        API_Request.send_data(config.Client.SEND_TEMP_DATA, data)
 
     def send_airquality(data):
-        try:
-            dataValue = {"dataValue": data}                                                             # Sensordaten die gesendet werden sollen --> Luftqualitaet
-
-            headers = {"APIKey": config.API_SEND_DATA.auth_token,                                       # Authentifizierung nötig zur Eingabe von Daten
-                   "Content-Type": "application/json"}
-        
-            response = requests.post(config.Client.SEND_AIRQUALITY_DATA, json=dataValue, headers=headers) # Befehl zum Senden an die API
-
-            API_Request.Statuscode(response.status_code)
-        except requests.exceptions.RequestException as e:
-            API_Request.Statuscode(e.response.status_code)
-
+        API_Request.send_data(config.Client.SEND_AIRQUALITY_DATA, data)
 
     def send_daynight(data):
-        try:
-
-            dataValue = {"dataValue": data}                                                             # Sensordaten die gesendet werden sollen --> Tag oder Nacht
-
-            headers = {"APIKey": config.API_SEND_DATA.auth_token,                                       # Authentifizierung nötig zur Eingabe von Daten
-                   "Content-Type": "application/json"}
-        
-            response = requests.post(config.Client.SEND_DAYNIGHT_DATA, json=dataValue, headers=headers) # Befehl zum Senden an die API
-
-            API_Request.Statuscode(response.status_code)
-        except requests.exceptions.RequestException as e:
-            API_Request.Statuscode(e.response.status_code)
+        API_Request.send_data(config.Client.SEND_DAYNIGHT_DATA, data)
